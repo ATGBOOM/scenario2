011835f67b0a8c4e75f11a3d4e916827a3e0990f
ATGBOOM <atgboom@Arnavs-MacBook-Pro.local>
2025-03-12 03:42:59 +0530

    added chatting

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -48,9 +48,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="chat"
         options={{
-          title: 'Tab Two',
+          headerShown : false,
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
